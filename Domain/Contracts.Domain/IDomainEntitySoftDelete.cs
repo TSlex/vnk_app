effe8f600339ead306e0b8873247b241a3f8c65f
aleksi_ttu <aleksi@ttu.ee>
2021-02-20 16:27:00 +0200

    contracts soft-update and soft-delete

---
+++ Domain/Contracts.Domain/IDomainEntitySoftDelete.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Contracts.Domain
+{
+    public interface IDomainEntitySoftDelete
+    {
+        string? DeletedBy { get; set; }
+        DateTime? DeletedAt { get; set; }
+    }
+}
---
+++ Domain/Contracts.Domain/IDomainEntitySoftUpdate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Contracts.Domain
+{
+    public interface IDomainEntitySoftUpdate: IDomainEntitySoftUpdate<long>
+    {
+    }
+    
+    public interface IDomainEntitySoftUpdate<TKey>
+    where TKey : IEquatable<TKey>
+    {
+        TKey MasterId { get; set; }
+    }
+}
