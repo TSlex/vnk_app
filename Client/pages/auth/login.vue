c0a0430a0e5bed9840556005885de341a41422dd
aleksi_ttu <aleksi@ttu.ee>
2021-03-24 18:17:23 +0200

    login page

--- Client/pages/auth/login.vue
+++ Client/pages/auth/login.vue
@@ -1,13 +1,29 @@
 <template>
-
+  <v-row justify="center" align-content="center" dense class="fill-height">
+    <v-col lg="4" md="6" sm="6">
+      <v-card class="text-center pa-10">
+        <h1 class="text-h4 font-weight-bold mb-4">Войти в систему</h1>
+        <span class="info--text">
+          <v-icon color="info" size="16">mdi-information-outline</v-icon>
+          Для регистрации обратитесь к администратору
+          </span>
+        <v-form class="mt-6">
+          <v-text-field label="Эл. адрес" required></v-text-field>
+          <v-text-field label="Пароль" required></v-text-field>
+          <v-checkbox label="Запомнить меня"></v-checkbox>
+          <v-btn rounded block large color="primary mb-6">Войти</v-btn>
+          <a href="#" class="text-decoration-none">На главную</a>
+        </v-form>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
 export default {
-  layout: "empty"
-}
+  layout: "auth",
+};
 </script>
 
 <style>
-
 </style>
