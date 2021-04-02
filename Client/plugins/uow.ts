9780d5489596a0f9ecf7f92cfc6e723438abedd9
aleksi_ttu <aleksi@ttu.ee>
2021-04-02 13:04:39 +0300

    uow base

--- Client/plugins/uow.ts
+++ Client/plugins/uow.ts
@@ -3,31 +3,31 @@ import { Inject } from "@nuxt/types/app";
 
 declare module 'vue/types/vue' {
   interface Vue {
-    $hello(message: string): void
+    $uow(message: string): void
   }
 }
 
 declare module '@nuxt/types' {
   interface NuxtAppOptions {
-    $hello(message: string): void
+    $uow(message: string): void
   }
   // nuxtContext.$myInjectedFunction
   interface Context {
-    $hello(message: string): void
+    $uow(message: string): void
   }
 }
 
 declare module 'vuex/types/index' {
   interface Store<S> {
-    $hello(message: string): void
+    $uow(message: string): void
   }
 }
 
-const MyPlugin: Plugin = (ctx: Context, inject: Inject) => {
+const AppUnitOfWork: Plugin = (ctx: Context, inject: Inject) => {
   const repositories = {
   }
 
-  inject('hello', (msg: string) => console.log(`Hello ${msg}!`))
+  inject('uow', (msg: string) => console.log(`Hello ${msg}!`))
 }
 
-export default MyPlugin
+export default AppUnitOfWork
