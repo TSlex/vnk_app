45d085412d6cd860ed8015f3d582aa6516367379
aleksi_ttu <aleksi@ttu.ee>
2021-04-02 19:02:03 +0300

    repo works!

---
+++ Client/dal/AttributeTypesRepo.ts
@@ -0,0 +1,30 @@
+import { AxiosInstance } from 'axios';
+
+export default ($axios: AxiosInstance) => ({
+  data() {
+    return {
+      resourseURL: "attributetypes/"
+    }
+  },
+
+  async getAll() {
+
+  },
+
+  async getById() {
+
+  },
+
+  async add() {
+
+  },
+
+  async edit() {
+
+  },
+
+  async delete() {
+
+  },
+
+})
