e6cac94262ea9c56430760fd895a94062a1ea66d
aleksi_ttu <aleksi@ttu.ee>
2021-05-18 21:38:57 +0300

    little fix
    Former-commit-id: 98c6b67a29105654bdb2b7546d67c7462bdfbde0

--- Server/AppAPI/1.0/Identity/LoginDTO.cs
+++ Server/AppAPI/1.0/Identity/LoginDTO.cs
@@ -4,10 +4,12 @@ namespace AppAPI._1._0.Identity
 {
     public class LoginDTO
     {
+        [MaxLength(50)]
         [Required]
         [EmailAddress]
         public string Email { get; set; } = default!;
-        
+
+        [MaxLength(50)]
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; } = default!;
